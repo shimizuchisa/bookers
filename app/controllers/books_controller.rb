38ed8b43af403cd421057e4ee1be1794569b13f6
shimizuchisa <112614370+shimizuchisa@users.noreply.github.com>
2023-10-25 02:16:27 +0000

    finish

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,49 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    # 直接にparams[:カラムの値]とならないのは、Railsがストロンパラメータを使っているから
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id]) #@追加　rederで値を表示させるため
+    if @book.update(book_params) #@追加
+      flash[:update] = "Book was successfully update."
+      redirect_to book_path(@book.id) #@追加
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
